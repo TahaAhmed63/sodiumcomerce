ff5ba196336be536832788eca1e2e23a17febb8e
TahaAhmed63 <tahakap234@gmail.com>
2025-01-26 03:04:11 +0500

    done

--- src/Components/homeComponents/Catagories.jsx
+++ src/Components/homeComponents/Catagories.jsx
@@ -1,3 +1,4 @@
+"use-client"
 import React, { useEffect, useState } from 'react'
 import Heading from '../mainComponents/heading'
 import { fetchCatagoiesData } from '@/store/slice/catagoriesSlice';
